44fbf8fb8c351f45b30e47000570d5e5ec466026
Liuba <kundasl1@gmail.com>
2023-04-23 01:10:28 +0300

    lesson_1 task 1-4

---
+++ lesson_1-4.py
@@ -0,0 +1,26 @@
+﻿"""
+# задание 4
+4. Преобразовать слова «разработка», «администрирование», «protocol», «standard» из строкового представления
+в байтовое
+ и выполнить обратное преобразование (используя методы encode и decode).
+
+"""
+def converter(i):
+    d = i.encode('UTF-8')
+    return d
+
+def converter_back(i):
+    d = i.decode('UTF-8')
+    return d
+
+if __name__ == "__main__":
+    #val = str("разработка")
+    val4 = ['разработка', 'администрирование', 'protocol', 'standard']
+    #val = 1
+    for i in val4:
+        print(converter(i))
+
+        new = converter(i)
+        print(converter_back(new))
+        print("***"*20)
+
---
+++ lesson_1_2-3.py
@@ -0,0 +1,41 @@
+﻿"""
+# задание 2
+2. Каждое из слов «class», «function», «method» записать в байтовом типе без преобразования в последовательность кодов
+ (не используя методы encode и decode) и определить тип, содержимое и длину соответствующих переменных.
+
+"""
+
+
+def bytes_type(i):
+    ib = bytes(i, 'UTF-8')
+    return ib
+
+
+def writer2(i):
+    print('тип переменной: {}\n содержимое переменной - {}\n длина переменной - {}'
+          .format(type(bytes_type(i)), bytes_type(i), len(bytes_type(i))))
+
+
+
+"""
+# задание 3
+3. Определить, какие из слов «attribute», «класс», «функция», «type» невозможно записать в байтовом типе.
+
+"""
+if __name__ == "__main__":
+    #task2
+    val2 = ['class', 'function', 'method']
+    for _ in val2:
+        writer2(_)
+        print('*' * 20)
+    #task 3
+    val3 = ['attribute', 'класс', 'функция', 'type']
+    for i in val3:
+        try:
+            #bytes(i, 'UTF-8')
+            bytes(i, 'ascii')
+
+        except UnicodeEncodeError:
+            print(f'Задание 3 ***\n Слово "{i}" невозможно записать в виде байтовой строки')
+
+#Задание 3 странное, если использовать UTF-8, то все преобразуется....((
