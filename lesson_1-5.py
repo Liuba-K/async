b391181e777407bd9d99b2084b61b4a38216ea3a
Liuba <kundasl1@gmail.com>
2023-04-24 00:10:09 +0300

    lesson_1 task 5 и 6

--- lesson_1-5.py
+++ lesson_1-5.py
@@ -1,13 +1,25 @@
-﻿
-"""
+﻿"""
 # задание 5
 
-5. Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты из байтовового в строковый тип на кириллице.
+5. Выполнить пинг веб-ресурсов yandex.ru, youtube.com и
+преобразовать результаты из байтовового в строковый тип на кириллице.
 """
+import subprocess
 
-"""
-# задание 6
-6. Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет», «декоратор».
- Проверить кодировку файла по умолчанию. Принудительно открыть файл в формате Unicode и вывести его содержимое.
 
-"""
+def ping(i):
+    subproc_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+    for i in subproc_ping.stdout:
+        #l = i.decode('ascii')
+        #print(i)
+        line = i.decode('cp866') #кириллицa
+        print(line)
+
+if __name__ == "__main__":
+    #args = ['ping', 'yandex.ru']
+    #ping(args)
+    webs = ['yandex.ru', 'youtube.com']
+    for web in webs:
+        args = ['ping', web]
+        ping(args)
+
