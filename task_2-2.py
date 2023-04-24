5a524ad067a038f4c96dae1fdef37c9987174bf0
Liuba <kundasl1@gmail.com>
2023-04-25 01:21:31 +0300

    lesson_2 modified task 2

--- task_2-2.py
+++ task_2-2.py
@@ -11,21 +11,27 @@
 import json
 
 def write_order_to_json(item, quantity, price, buyer, date):
-    Dict_ex = {
-        "item": item,
-        "quantity": quantity,
-        "price": price,
-        "buyer": buyer,
-        "date": date
-    }
+
     with open('orders.json', 'r', encoding='utf-8') as f_out:
         data = json.load(f_out)
 
-        with open('orders-1.json', 'w', encoding='utf-8') as f_n:
-            data['orders'].append(Dict_ex)
-            json.dump(data, f_n)
+    with open('orders-1.json', 'w', encoding='utf-8') as f_n:
+            list_orders = data["orders"]
+            Dict_ex = {
+                "item": item,
+                "quantity": quantity,
+                "price": price,
+                "buyer": buyer,
+                "date": date
+            }
+            list_orders.append(Dict_ex)
+            json.dump(data, f_n, indent=4, ensure_ascii=False)
+        #отступ indent
+        #ensure_ascii = False, строки запишутся как есть
+    #w-перезаписываем только последние данные
 
 
+write_order_to_json("book", 2, 10.50, "Lisa", "22-10-2022")
+write_order_to_json("компьютер", 5, 25, "Sidorov", "02-05-2022")
 
-write_order_to_json('book', 2, 10.50, 'Lisa', "22-10-2022")
 
