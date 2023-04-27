52aed19e9e02c0d2a63b96277c524136d252a58a
Liuba <kundasl1@gmail.com>
2023-04-27 22:14:41 +0300

    lesson_3 client-server (design code)

--- client.py
+++ client.py
@@ -1,8 +1,14 @@
 ﻿from socket import *
 
+def main():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(('localhost', 10000))
+    tm = s.recv(1024)
+    s.close()
+    print("текущее время: ", tm.decode('utf-8'))
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect(('localhost', 10000))
-tm = s.recv(1024)
-s.close()
-print("текущее время: ", tm.decode('utf-8'))
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
