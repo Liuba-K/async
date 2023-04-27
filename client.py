a49766f83be4313201ae83420ab1527f6a18fb3c
Liuba <kundasl1@gmail.com>
2023-04-27 19:02:39 +0300

    lesson_3 client-setver

---
+++ client.py
@@ -0,0 +1,8 @@
+﻿from socket import *
+
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(('localhost', 10000))
+tm = s.recv(1024)
+s.close()
+print("текущее время: ", tm.decode('utf-8'))
---
+++ server.py
@@ -0,0 +1,13 @@
+﻿from socket import *
+import time
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('', 10000))
+s.listen(5)#подключается 5 клиентов
+
+while True:
+    client, addr = s.accept()
+    print('Получаем запрос на соединение:', addr)
+    timestr = time.time(time.time()) + '\n'
+    client.send(timestr.encode('utf-8')) #ajhvbhetncz cjj,otybt
+    client.close()
