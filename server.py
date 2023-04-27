0d61c3a48e9c79db76c07c11144f31ccb5ddeaea
Liuba <kundasl1@gmail.com>
2023-04-27 22:14:41 +0300

    lesson_3 client-server (design code)

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
 ﻿from socket import *
 import time
 
-s = socket(AF_INET, SOCK_STREAM)
-s.bind(('', 10000))
-s.listen(5)#подключается 5 клиентов
+def main():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(('', 10000))
+    s.listen(5)#подключается 5 клиентов
 
-while True:
-    client, addr = s.accept()
-    print('Получаем запрос на соединение:', addr)
-    timestr = time.time(time.time()) + '\n'
-    client.send(timestr.encode('utf-8')) #ajhvbhetncz cjj,otybt
-    client.close()
+    while True:
+        client, addr = s.accept()
+        print('Получаем запрос на соединение:', addr)
+        timestr = time.ctime(time.time()) + '\n'
+        client.send(timestr.encode('utf-8')) #обязательна кодировка
+        client.close()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e) #traceback
