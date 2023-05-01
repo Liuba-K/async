33c72d443c870e058cd58863f14666fb0668fbfb
Liuba <kundasl1@gmail.com>
2023-05-01 16:12:17 +0300

    lesson_3 client-server (JIM)

--- client_JIM.py
+++ client_JIM.py
@@ -1,94 +1,38 @@
-﻿"""Программа-клиент"""
-
-import sys
+﻿from socket import *
 import json
-import socket
 
 
-def create_presence(account_name='Guest'):
-    '''
-    Функция генерирует запрос о присутствии клиента
-    :param account_name:
-    :return:
-    '''
-    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
-    out = {
-        'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}
-    }
-    return out
-
-
-def process_ans(message):
-    '''
-    Функция разбирает ответ сервера
-    :param message:
-    :return:
-    '''
-    if 'response' in message:
-        if message['response'] == 200:
-            return '200 : OK'
-        return f'400 : error'
-    raise ValueError
+def create_presence_messages():
+    pass
 
 
 def main():
-    '''Загружаем параметы коммандной строки'''
-    # client.py 192.168.1.2 8079
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = '127.0.0.1'
-        server_port = 7777
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(('localhost', 7777))
+    msg = '''{
+    "action": "presence",
+    "time":1682704782.6792958,
+    "user": {
+    "Имя":
+    "C0deMaver1ck",
+    "password":
+    "CorrectHorseBatteryStaple"
+    }
+    }'''
 
-    # Инициализация сокета и обмен
+    # msg = 'Привет, сервер'.encode('utf-8')
+    msg_json = json.dumps(msg)
+    msg_encode = msg_json.encode('utf-8')
+    s.send(msg_encode)
+    # s.send(msg_json)
+    tm = s.recv(1024)
 
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = create_presence()
-    send_message(transport, message_to_server)
-    try:
-        answer = process_ans(get_message(transport))
-        print(answer)
-    except (ValueError, json.JSONDecodeError):
-        print('Не удалось декодировать сообщение сервера.')
-
-def get_message(client):
-    '''
-    Утилита приёма и декодирования сообщения
-    принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
-    :param client:
-    :return:
-    '''
-
-    encoded_response = client.recv(1024)
-    if isinstance(encoded_response, bytes):
-        json_response = encoded_response.decode('utf-8')
-        response = json.loads(json_response)
-        if isinstance(response, dict):
-            return response
-        raise ValueError
-    raise ValueError
-
-
-def send_message(sock, message):
-    '''
-    Утилита кодирования и отправки сообщения
-    принимает словарь и отправляет его
-    :param sock:
-    :param message:
-    :return:
-    '''
-
-    js_message = json.dumps(message)
-    encoded_message = js_message.encode('utf-8')
-    sock.send(encoded_message)
+    s.close()
+    print("Сервер: ", tm.decode('utf-8'))
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e)
--- server_JIM.py
+++ server_JIM.py
@@ -1,115 +1,62 @@
-﻿"""Программа-сервер"""
-
-import socket
-import sys
+﻿from socket import *
+import time
 import json
 
-def process_client_message(message):
-    '''
-    Обработчик сообщений от клиентов, принимает словарь -
-    сообщение от клинта, проверяет корректность,
-    возвращает словарь-ответ для клиента
 
-    :param message:
-    :return:
-    '''
-    if 'action' in message and message['action'] == 'presence' and 'time' in message \
-            and 'user' in message and message['user']['account_name'] == 'Guest':
+def accept_client_message(message):
+    #принимает сообщение клиента
+    if message['action'] == 'presence' and 'time' in message:
         return {'response': 200}
     return {
         'response': 400,
-        'error': 'Bad Request'
+        'error': 'Bad Request' #необязательное поле
     }
 
 
 def main():
-    '''
-    Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
-    Сначала обрабатываем порт:
-    server.py -p 8079 -a 192.168.1.2
-    :return:
-    '''
 
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = 7777
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта.')
-        sys.exit(1)
-    except ValueError:
-        print(
-            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
-
-    # Затем загружаем какой адрес слушать
-
-    try:
-        if '-a' in sys.argv:
-            listen_address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            listen_address = ''
-
-    except IndexError:
-        print(
-            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
-        sys.exit(1)
-
-    # Готовим сокет
-
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.bind((listen_address, listen_port))
-
-    # Слушаем порт
-
-    transport.listen(5)
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(('localhost', 7777))
+    s.listen(5)#подключается 5 клиентов
 
     while True:
-        client, client_address = transport.accept()
-        try:
-            message_from_cient = get_message(client)
-            print(message_from_cient)
-            # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
-            response = process_client_message(message_from_cient)
-            send_message(client, response)
-            client.close()
-        except (ValueError, json.JSONDecodeError):
-            print('Принято некорретное сообщение от клиента.')
-            client.close()
-def get_message(client):
-    '''
-    Утилита приёма и декодирования сообщения
-    принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
-    :param client:
-    :return:
-    '''
+        client, addr = s.accept()
+        print('Получаем запрос на соединение:', addr)
+        data = client.recv(1024)
 
-    encoded_response = client.recv(1024)
-    if isinstance(encoded_response, bytes):
-        json_response = encoded_response.decode('utf-8')
-        response = json.loads(json_response)
-        if isinstance(response, dict):
-            return response
-        raise ValueError
-    raise ValueError
+        json.loads(data.decode('utf-8'))
+        print('Было получено сообщение: ', json.loads(data.decode('utf-8')))
+        #print(data) #str в байтах b'
+        #print(data.decode('utf-8')) #как есть в клиенте
+        print('Было получено сообщение: ', json.loads(data))
 
+        msg = 'Ваше сообщение получено'     #то, что видит клиент
+        client.send(msg.encode('utf-8')) #отправляем в байтах и только первое..
 
-def send_message(sock, message):
-    '''
-    Утилита кодирования и отправки сообщения
-    принимает словарь и отправляет его
-    :param sock:
-    :param message:
-    :return:
-    '''
+        #timestr = time.ctime() + '\n'
+        #client.send(timestr.encode('utf-8')) #обязательна кодировка
+        client.close()
+'''{
+"action": "authenticate",
+"time":1682704782.6792958,
+"user": {
+"account_name":
+"C0deMaver1ck",
+"password":
+"CorrectHorseBatteryStaple"
+}
+}'''
 
-    js_message = json.dumps(message)
-    encoded_message = js_message.encode('utf-8')
-    sock.send(encoded_message)
-
+'''
+def do_action(action):
+    if action == "authenticate":
+        pass
+        # do authenticate
+    return result
+'''
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(e) #traceback
