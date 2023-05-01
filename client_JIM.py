da2b14fd3387aad2beca57a123fd3ce8a5c5325b
Liuba <kundasl1@gmail.com>
2023-05-01 19:00:40 +0300

    lesson_3 client-server (JIM) port 7777 and localhost

--- client_JIM.py
+++ client_JIM.py
@@ -1,34 +1,68 @@
 ﻿from socket import *
 import json
+import sys
 
 
 def create_presence_messages():
-    pass
+    #сформировать presence-сообщение
+    messenger = {
+       "action": "presence",
+       "time": 1682704782.6792958,
+       "user": {
+               "Имя":
+               "C0deMaver1ck",
+               "password":
+               "CorrectHorseBatteryStaple"
+               }
+       }
+    return messenger
 
 
-def main():
-    s = socket(AF_INET, SOCK_STREAM)
-    s.connect(('localhost', 7777))
-    msg = '''{
-    "action": "presence",
-    "time":1682704782.6792958,
-    "user": {
-    "Имя":
-    "C0deMaver1ck",
-    "password":
-    "CorrectHorseBatteryStaple"
-    }
-    }'''
-
-    # msg = 'Привет, сервер'.encode('utf-8')
+def send_message(s, msg):
+    #отправить сообщение серверу
     msg_json = json.dumps(msg)
     msg_encode = msg_json.encode('utf-8')
     s.send(msg_encode)
-    # s.send(msg_json)
-    tm = s.recv(1024)
+
+
+def answer_server(s):
+    #получить ответ сервера
+    encoded_response = s.recv(1024)
+    print(encoded_response)
+
+    if isinstance(encoded_response, bytes):
+        json_response = encoded_response.decode('utf-8')
+        response = json.loads(json_response)
+        if isinstance(response, dict):
+            return response
+        raise ValueError
+    raise ValueError
+def process_message_server(message):
+    #разобрать сообщение сервера;
+    if message['response'] == 200:
+        return '200: ОК'
+    else:
+        return message['error'], message['response']
+
+    #могут быть и другие трехзначные response
+
+def main():
+    #параметры командной строки скрипта client.py <addr> [<port>]:
+    addr = sys.argv[1]
+    port = int(sys.argv[2])
+
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((addr, port)) ##127.0.0.1 7777
+
+    msg = create_presence_messages()
+
+    send_message(s, msg)
+
+    answer = process_message_server(answer_server(s))
+    print(answer)
 
     s.close()
-    print("Сервер: ", tm.decode('utf-8'))
+
 
 
 if __name__ == '__main__':
--- server_JIM.py
+++ server_JIM.py
@@ -1,7 +1,7 @@
 ﻿from socket import *
 import time
 import json
-
+from client_JIM import answer_server
 
 def accept_client_message(message):
     #принимает сообщение клиента
@@ -14,46 +14,30 @@ def accept_client_message(message):
 
 
 def main():
+    listen_port = 7777
+    listen_address = ''
 
     s = socket(AF_INET, SOCK_STREAM)
-    s.bind(('localhost', 7777))
+    s.bind((listen_address, listen_port))
     s.listen(5)#подключается 5 клиентов
 
     while True:
         client, addr = s.accept()
-        print('Получаем запрос на соединение:', addr)
-        data = client.recv(1024)
+        #print(answer_server(client))
+        print('Получаем запрос на соединение:', client)
+        #data = client.recv(1024)
 
-        json.loads(data.decode('utf-8'))
-        print('Было получено сообщение: ', json.loads(data.decode('utf-8')))
-        #print(data) #str в байтах b'
-        #print(data.decode('utf-8')) #как есть в клиенте
-        print('Было получено сообщение: ', json.loads(data))
-
+        #json.loads(data.decode('utf-8'))
+        #print('Было получено сообщение: ', json.loads(data.decode('utf-8')))
+        message_from_cient = answer_server(client)
+        print(message_from_cient)
         msg = 'Ваше сообщение получено'     #то, что видит клиент
         client.send(msg.encode('utf-8')) #отправляем в байтах и только первое..
 
         #timestr = time.ctime() + '\n'
         #client.send(timestr.encode('utf-8')) #обязательна кодировка
         client.close()
-'''{
-"action": "authenticate",
-"time":1682704782.6792958,
-"user": {
-"account_name":
-"C0deMaver1ck",
-"password":
-"CorrectHorseBatteryStaple"
-}
-}'''
 
-'''
-def do_action(action):
-    if action == "authenticate":
-        pass
-        # do authenticate
-    return result
-'''
 
 if __name__ == '__main__':
     try:
