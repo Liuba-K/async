d1b39d0edede2af96837444b3044628a0d350d37
Liuba <kundasl1@gmail.com>
2023-05-06 00:20:03 +0300

    lesson_3 server:recv, answer, ip+port

--- server_JIM.py
+++ server_JIM.py
@@ -1,8 +1,9 @@
-﻿import socket
+﻿#import socket
 from socket import *
 import time
 import json
 import sys
+import argparse
 
 MAX_MSG_LEN = 640
 MAX_SYMBOL_LEN_IN_BYTES = 4
@@ -14,8 +15,10 @@ def accept_client_message(message):
 
 def read_message(s: socket):
     #получить ответ сервера
+    """
     encoded_response = s.recv(MAX_MSG_LEN * MAX_SYMBOL_LEN_IN_BYTES) #проверка на длину 640символов
-
+    """
+    encoded_response = s.recv(1024)
     json_response = encoded_response.decode('utf-8')
     print(f'response from server: {json_response}')
     if len(json_response) > MAX_MSG_LEN:
@@ -30,16 +33,19 @@ def read_message(s: socket):
 
 def main():
 
-    listen_port = 7777
-    listen_address = ''
-
     s = socket(AF_INET, SOCK_STREAM)
-    s.bind((listen_address, listen_port))
+
+    parser = argparse.ArgumentParser("port and address")
+    parser.add_argument('-p', nargs='?', default='7777')
+    parser.add_argument('-a', nargs='?', help='Input addr ', default='127.0.0.1')
+
+    namespace = parser.parse_args(sys.argv[1:])
+
+    s.bind((namespace.a, namespace.p)) #listen_address, listen_port
     s.listen(5)#подключается 5 клиентов
 
     while True:
         client, addr = s.accept()
-        #print(answer_server(client))
         print('Получаем запрос на соединение:', client)
         #data = client.recv(1024)
 
@@ -47,11 +53,9 @@ def main():
         #print('Было получено сообщение: ', json.loads(data.decode('utf-8')))
         message_from_client = read_message(client)
         print(message_from_client)
-        msg = 'Ваше сообщение получено'     #то, что видит клиент
-        client.send(msg.encode('utf-8')) #отправляем в байтах и только первое..
+        msg = accept_client_message()    #то, что видит клиент
+        client.send(json.dumps(msg).encode('utf-8')) #отправляем в байтах и только первое..
 
-        #timestr = time.ctime() + '\n'
-        #client.send(timestr.encode('utf-8')) #обязательна кодировка
         client.close()
 
 
@@ -59,4 +63,4 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        print(e) #traceback
+        print('failed to start server') #traceback
