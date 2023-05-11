4483a386e4ff6ef0620eacaae6da5c0dad3a51fc
Liuba <kundasl1@gmail.com>
2023-05-11 19:47:53 +0300

    lesson_7 module select

---
+++ client.py
@@ -0,0 +1,22 @@
+﻿from socket import *
+
+def main():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(('localhost', 10000))
+    while True:
+        tm = s.recv(1024)
+        print("текущее время: ", tm.decode('utf-8'))
+
+    s.close()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,67 @@
+﻿from socket import socket, AF_INET, SOCK_STREAM
+import time
+import select
+
+
+def listen_socket(address):
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind(('', 10000))
+    sock.listen(5)
+    sock.settimeout(0.2)
+
+    return sock
+
+
+def mainloop():
+    address = (('', 10000))
+    clients = []
+    sock = listen_socket(address)
+
+    while True:
+        try:
+            client, addr = sock.accept()
+        except OSError as e:
+            pass
+        else:
+            print('Получаем запрос на соединение:', addr)
+            clients.append(client)
+        finally:
+            recv_lst = []
+            send_lst = []
+            err_lst = []
+
+            #w = []
+            try:
+                if clients:
+                    recv_lst, send_lst, err_lst = select.select([], clients, [], 0)
+            except Exception as e:
+                pass
+            if recv_lst:
+                for client_with_message in recv_lst:
+
+
+
+                """ 
+                            mes = input()
+                            if mes == 'r':
+                                client.set_read()
+                            elif mes == 'w':
+                                client.set_write()
+                                """
+                for s_client in w:
+                    timestr = time.ctime(time.time()) + '\n'
+                    try:
+                        s_client.send(timestr.encode('utf-8'))
+                    except:
+                        clients.remove(s_client)
+
+
+print("запущен")
+mainloop()
+""" 
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e) #traceback
+"""
