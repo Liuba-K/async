b8d9edfcc32ee76c4d7c8850e54befa52c1e8b9e
Liuba <kundasl1@gmail.com>
2023-05-19 01:20:46 +0300

    lesson_9 tabulate

---
+++ task_3.py
@@ -0,0 +1,10 @@
+﻿"""
+3. Написать функцию host_range_ping_tab(), возможности которой основаны на функции из примера 2.
+Но в данном случае результат должен быть итоговым по всем ip-адресам, представленным в табличном формате
+(использовать модуль tabulate). Таблица должна состоять из двух колонок и выглядеть примерно так:
+"""
+from tabulate import tabulate
+columns = ['programming language', 'type', 'year']
+tuples_list = [('Python', 'interpreted', '1991'), ('JAVA', 'compiled', '1995'), ('С', 'compiled', '1972')]
+print(tabulate(tuples_list))
+print(tabulate(tuples_list, headers=columns, tablefmt="grid", stralign="center"))
