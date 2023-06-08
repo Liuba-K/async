4d1196fb9dc2d8d13388c9b77f28a4cebd0fed23
Liuba <kundasl1@gmail.com>
2023-06-08 23:09:11 +0300

    lesson_13 added GUI

--- gui.py
+++ gui.py
@@ -12,21 +12,71 @@ from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QTableView, QDialog, QFi
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtCore import Qt
 import os
+# GUI - Функция реализующая заполнение таблицы историей сообщений.
+def create_stat_model(database):
+    # Список записей из базы
+    hist_list = database.message_history()
 
+    # Объект модели данных:
+    listcontact = QStandardItemModel()
+    listcontact.setHorizontalHeaderLabels(
+        ['Имя Клиента', 'Последний раз входил', 'Сообщений отправлено', 'Сообщений получено'])
+    for row in hist_list:
+        user, last_seen, sent, recvd = row
+        user = QStandardItem(user)
+        user.setEditable(False)
+        last_seen = QStandardItem(str(last_seen.replace(microsecond=0)))
+        last_seen.setEditable(False)
+        sent = QStandardItem(str(sent))
+        sent.setEditable(False)
+        recvd = QStandardItem(str(recvd))
+        recvd.setEditable(False)
+        list.appendRow([user, last_seen, sent, recvd])
+    return list
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.initGUI() #вынести отдельна параметры
+
+    def initGui(self):
         self.setWindowTitle('список контактов')
-        self.resize(300,100)
+        self.setFixedSize(800, 600)
+        #self.resize(300,100)
         layout = QVBoxLayout()
 
-        layout.addWidget()
-        self.setLayout(layout)
+        #кнопки
+        self.refresh_button = QAction('Обновить список', self)
+        self.config_button = QAction('Настройки сервера', self)
+        self.show_history_button = QAction('История клиентов', self)
+
+        self.statusBar()
+        self.toolbar = self.addToolBar('MainBar')
+        self.toolbar.addAction(QAction('Выход', self)) #требуются дополнительные параметры
+        self.toolbar.addAction(self.refresh_button)
+        self.toolbar.addAction(self.show_history_button)
+        self.toolbar.addAction(self.config_btn)
+
+        # Надпись о том, что ниже список подключённых клиентов
+        self.label = QLabel('Список подключённых клиентов:', self)
+        self.label.setFixedSize(240, 15)
+        self.label.move(10, 25)
+
+        # Окно со списком подключённых клиентов.
+        self.active_clients_table = QTableView(self)
+        self.active_clients_table.move(10, 45)
+        self.active_clients_table.setFixedSize(780, 400)
+        self.show()
+        #layout.addWidget()
+        #self.setLayout(layout)
 
 if __name__ == "__main__":
     import sys
 
     app = QApplication(sys.argv)
     window = MainWindow()
-    window.show()
+
+    #window.statusBar().showMessage('Test Statusbar Message')
+
+    #window.show()
     sys.exit(app.exec())
+    #app.exec_()
